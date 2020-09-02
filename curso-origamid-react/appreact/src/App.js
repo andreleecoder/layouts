2e8e36aa211b04557f5e7f55e1b2c54df2782aca
Andre Lee <andreleecoder@gmail.com>
2020-09-02 00:39:25 -0300

    jsx e exercicios

---
+++ curso-origamid-react/appreact/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+// Mostre os dados da aplicação, como aprensetado no vídeo
+// Não utilize CSS externo, use o style para mudar as cores
+// Se a situação estiver ativa pinte de verde, inativa vermelho
+// Se o gasto for maior que 10000 mostre uma mensagem
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
+
+const App = () => {
+  const dados = mario;
+  const total = dados.compras
+    .map((item) => Number(item.preco.replace('R$ ', '')))
+    .reduce((a, b) => a + b);
+
+  return (
+    <>
+      <p>Nome: {dados.cliente}</p>
+      <p>idade: {dados.idade}</p>
+      <p>
+        situação:{' '}
+        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
+          {dados.ativa ? 'ativa' : 'inativa'}
+        </span>
+      </p>
+      <p>Total: R${total}</p>
+      {total > 1000 && <p>Você está gsstando muito</p>}
+    </>
+  );
+};
+
+export default App;
